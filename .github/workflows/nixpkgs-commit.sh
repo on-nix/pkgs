16f365ad130d3ae498266ecd13951eeb7908bd62
Kevin Amado <kamadorueda@gmail.com>
2021-10-23 18:44:04 -0500

    feat(build): improve error handling

--- .github/workflows/nixpkgs-commit.sh
+++ .github/workflows/nixpkgs-commit.sh
@@ -10,19 +10,16 @@ count=0
 for commit in "${commits[@]}"; do
   target="data/nixpkgs/commits/${commit}.json"
 
-  if ! test -e "${target}"; then
-    src="https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz"
-    if nix-env -qaf "${src}" --json > tmp; then
-      jq -er 'to_entries|map({(.key):.value.version})|add' < tmp > "${target}"
-    else
-      echo "{}" > "${target}"
-    fi
+  # Skip this commit if we already processed it
+  if test -e "${target}"; then continue; fi
+  # Stop after processing 25 commits
+  if test "${count}" -gt 25; then break; else count="$((count + 1))"; fi
 
-    if test "${count}" = 25; then
-      break
-    else
-      count="$((count + 1))"
-    fi
+  # Query available packages in this commit or save an empty set
+  src="https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz"
+  query='to_entries|map({(.key):.value.version})|add'
+  if ! nix-env -qaf "${src}" --json | jq -er "${query}" > "${target}"; then
+    echo "{}" > "${target}"
   fi
 done
 
